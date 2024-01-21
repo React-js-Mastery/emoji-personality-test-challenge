f696d1325a55ee60be14c4da561c05a759fb264c
Ajay-Dhangar <ajaydhangar49@gmail.com>
2024-01-21 11:02:49 +0530

    Emoji Personality Test Challenge created

---
+++ App.jsx
@@ -0,0 +1,90 @@
+import React from "react"
+import ResultsModal from "./components/ResultsModal"
+import EmojiLists from "./components/EmojiLists"
+import emojis from "./data/emojis"
+import { nanoid } from "nanoid"
+
+export default function App() {
+	const [likedEmojis, setLikedEmojis] = React.useState([])
+	const [passedEmojis, setPassedEmojis] = React.useState([])
+	const [currentEmojis, setCurrentEmojis] = React.useState(getRandomEmojis)
+	const [showResults, setShowResults] = React.useState(false)
+	const [resultsReady, setResultsReady] = React.useState(false)
+
+	function handleClick(event) {
+		
+		const clickedEmoji = event.target.textContent
+		
+		const notClickedEmojis = currentEmojis.filter(emoji => emoji !== clickedEmoji)
+				
+		setLikedEmojis(prevLikedEmojis => [...prevLikedEmojis, clickedEmoji])
+		setPassedEmojis(prevPassedEmojis => [...prevPassedEmojis, ...notClickedEmojis])
+		setCurrentEmojis(getRandomEmojis)
+	
+	}
+	
+
+	function getRandomEmojis() {
+		function chooseRandomEmoji() {
+			return emojis[Math.floor(Math.random() * emojis.length)]
+		}
+		return new Array(3).fill("").map((item) => chooseRandomEmoji())
+	}
+
+	function getResults() {
+		setShowResults(true)
+	}
+
+	function reset() {
+		setLikedEmojis([])
+		setPassedEmojis([])
+		setShowResults(false)
+		setResultsReady(false)
+	}
+
+	React.useEffect(() => {
+		showResults &&
+			setTimeout(() => {
+				setResultsReady(true)
+			}, 2000)
+	}, [showResults])
+
+	function generateListItems(element) {
+		return <li key={nanoid()}>{element}</li>
+	}
+
+	return (
+		<div className="wrapper">
+			<div className="results-counter">{likedEmojis.length} / 10</div>
+
+			<ResultsModal
+				showResults={showResults}
+				getResults={getResults}
+				resultsReady={resultsReady}
+				reset={reset}
+				generateListItems={generateListItems}
+				likedEmojis={likedEmojis}
+			/>
+
+			<h1>Emoji Personality Test</h1>
+
+			<div className="overall-emojis-container">
+				<button onClick={handleClick}>
+					{currentEmojis[0]}
+				</button>
+				<button onClick={handleClick}>
+					{currentEmojis[1]}
+				</button>
+				<button onClick={handleClick}>
+					{currentEmojis[2]}
+				</button>
+			</div>
+
+			<EmojiLists
+				likedEmojis={likedEmojis}
+				passedEmojis={passedEmojis}
+				generateListItems={generateListItems}
+			/>
+		</div>
+	)
+}
